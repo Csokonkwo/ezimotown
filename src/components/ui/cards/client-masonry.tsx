8761d0f53fcc7fe4f0b61cfe0eff44da0463118a
samcesa45 <samcesa45@gmail.com>
2025-07-05 18:48:01 +0100

    minor fix

--- src/components/ui/cards/client-masonry.tsx
+++ src/components/ui/cards/client-masonry.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
-import { motion } from 'framer-motion';
+import { motion } from 'motion/react';
 import Image from 'next/image';
 import { gridItems } from '@/constants';
 
